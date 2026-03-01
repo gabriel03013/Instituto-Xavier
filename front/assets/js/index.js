e047d44c488110d188350ab75ddb77120dfb3dd9
gabriel03013 <gm5770283@gmail.com>
2026-03-01 20:23:16 -0300

    feat: login api integration/ home api integration

--- front/assets/js/index.js
+++ front/assets/js/index.js
@@ -1,21 +1,23 @@
 import { api } from "./utils.js";
 const form = document.querySelector("form");
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const [user, senha] = form.querySelectorAll("input");
 
-//   try {
-//     const res = api("login", "POST", {
-//       user,
-//       senha,
-//     });
+  try {
+    const res = await api("token", "POST", {
+      username: user.value,
+      password: senha.value,
+    });
 
-//     window.location.href = `./pages/${res.tipo.toLowerCase()}/index.html?tipo=${res.tipo.toLowerCase()}&id=${res.id}`
-//   } catch (e) {
-//     document.getElementById("erro").style.display = "flex";
-//   }
+    localStorage.setItem("token", res.access_token);
+    localStorage.setItem("userId", res.id);
+    localStorage.setItem("userTipo", res.tipo);
 
-    window.location.href = "./pages/admin/index.html";
+    window.location.href = `./pages/${res.tipo.toLowerCase()}/index.html?tipo=${res.tipo.toLowerCase()}&id=${res.id}`;
+  } catch (e) {
+    document.getElementById("erro").style.display = "flex";
+  }
 });
--- front/assets/js/utils.js
+++ front/assets/js/utils.js
@@ -1,12 +1,29 @@
 export async function api(endpoint, metodo = "GET", body = null) {
-  const res = await fetch(`apiUrl/${endpoint}`, {
-    method: metodo,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: body ? JSON.stringify(body) : null,
-  });
+  const headers = {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  };
 
+  let processedBody = body;
+  if (
+    body &&
+    !(body instanceof FormData) &&
+    !(body instanceof URLSearchParams)
+  ) {
+    if (endpoint === "token") {
+      headers["Content-Type"] = "application/x-www-form-urlencoded";
+      processedBody = new URLSearchParams(body).toString();
+    } else {
+      headers["Content-Type"] = "application/json";
+      processedBody = JSON.stringify(body);
+    }
+  }
+
+  const res = await fetch(`http://127.0.0.1:8000/${endpoint}`, {
+    method: metodo,
+    headers: headers,
+    body: processedBody,
+  });
+  
   if (!res.ok) throw new Error("Erro na requisição");
 
   return res.json();
